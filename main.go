a1520592d3774723c11c0ca18c1201b25b69d486
Denis Smirnov <113039356+VenirWeber@users.noreply.github.com>
2023-09-18 23:01:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"https://github.com/VenirWeber/listgo/blob/main/storage/list.go"
-)
-
-func main() {
-	myList := &List{}
-	myList.Add(42)
-	myList.Add(123)
-	myList.Add(7)
-	myList.Add(123) // Добавляем элемент со значением 123 второй раз
-
-	ids, ok := myList.GetAllByValue(123) // Получение индексов элементов со значением 123
-
-	if ok {
-		fmt.Println("Индексы элементов:", ids)
-	} else {
-		fmt.Println("Элементы с указанным значением не найдены.")
-	}
-
-	ids, ok = myList.GetAllByValue(99) // Попытка получить индексы элементов со значением 99 (не существует)
-
-	if ok {
-		fmt.Println("Индексы элементов:", ids)
-	} else {
-		fmt.Println("Элементы с указанным значением не найдены.")
-	}
-}
+func main() {}
