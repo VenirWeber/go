9d1abd62e991e53de8c28cffbba5e2716cdac119
Denis Smirnov <113039356+VenirWeber@users.noreply.github.com>
2023-09-19 00:56:24 +0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"C:\Users\Denchik\goprj\src\list\storage\list.go"
+)
+
+func main() {
+	myList := &List{}
+	myList.Add(42)
+	myList.Add(123)
+	myList.Add(7)
+	myList.Add(123) // Добавляем элемент со значением 123 второй раз
+
+	ids, ok := myList.GetAllByValue(123) // Получение индексов элементов со значением 123
+
+	if ok {
+		fmt.Println("Индексы элементов:", ids)
+	} else {
+		fmt.Println("Элементы с указанным значением не найдены.")
+	}
+
+	ids, ok = myList.GetAllByValue(99) // Попытка получить индексы элементов со значением 99 (не существует)
+
+	if ok {
+		fmt.Println("Индексы элементов:", ids)
+	} else {
+		fmt.Println("Элементы с указанным значением не найдены.")
+	}
+}
