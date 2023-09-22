ffebf5005b86431085d88c12ef7895a12a6c7fe5
Denis Smirnov <113039356+VenirWeber@users.noreply.github.com>
2023-09-22 11:12:15 +0300

    Update list.go

--- storage/list/list.go
+++ storage/list/list.go
@@ -20,8 +20,7 @@ func (l *List) Add(value int64) (id int64) {
 	newNode := &node{
 		value: value,
 		next:  nil,
-	} /* Создаем новый узел с заданным значением value
-	и добавляем его в конец списка. */
+	}
 
 	if l.firstNode == nil {
 		l.firstNode = newNode
@@ -31,26 +30,20 @@ func (l *List) Add(value int64) (id int64) {
 			current = current.next
 		}
 		current.next = newNode
-	} /*Если список пустой, устанавливаем новый узел как первый узел.
-	В противном случае, мы перебираем список, пока не найдем последний узел,
-	и добавляем новый узел в конец списка.*/
+	}
 
 	l.len++
-	return l.len - 1 /* Возвращает индекс добавленного элемента,
-	который равен текущей длине списка - 1 (т.к. индексация начинается с 0).*/
+	return l.len - 1
 } // Добавляет элемент в список и возвращает его индекс
 
 func (l *List) RemoveByIndex(id int64) {
 	if id < 0 || id >= l.len {
 		return
-	} // Проверяем, что индекс находится в допустимых пределах
+	}
 
 	if id == 0 {
-		// Если удаляемый элемент - первый в списке, переназначаем первый узел
 		l.firstNode = l.firstNode.next
 	} else {
-		/* В противном случае, находим узел перед удаляемым узлом
-		   и переназначаем указатель на следующий узел*/
 		current := l.firstNode
 		for i := int64(0); i < id-1; i++ {
 			current = current.next
@@ -58,31 +51,29 @@ func (l *List) RemoveByIndex(id int64) {
 		current.next = current.next.next
 	}
 
-	l.len-- /* Удаляем элемент из списка, обновляя указатели
-	узлов таким образом, чтобы исключить удаляемый элемент.*/
+	l.len--
 } // Удаляет элемент из списка по индексу
 
 func (l *List) RemoveByValue(value int64) {
 	if l.len == 0 {
 		return
-	} // Обработка случая, когда список пуст
+	}
 
 	if l.firstNode.value == value {
 		l.firstNode = l.firstNode.next
 		l.len--
 		return
-	} // Если удаляемый элемент находится в начале списка
+	}
 
 	current := l.firstNode
 	for current.next != nil && current.next.value != value {
 		current = current.next
-	} // Поиск элемента для удаления
+	}
 
 	if current.next == nil {
 		return
-	} // Если элемент не найден, выходим
+	}
 
-	// Удаляем элемент, переназначая указатель на следующий узел
 	current.next = current.next.next
 	l.len--
 } // Удаление элемента из списка по значению
@@ -90,12 +81,12 @@ func (l *List) RemoveByValue(value int64) {
 func (l *List) RemoveAllByValue(value int64) {
 	if l.len == 0 {
 		return
-	} // Обработка случая, когда список пуст
+	}
 
 	for l.firstNode != nil && l.firstNode.value == value {
 		l.firstNode = l.firstNode.next
 		l.len--
-	} // Удаляем нужные элементы в начале списка если такие есть
+	}
 
 	current := l.firstNode
 	for current != nil && current.next != nil {
@@ -105,12 +96,12 @@ func (l *List) RemoveAllByValue(value int64) {
 		} else {
 			current = current.next
 		}
-	} // Поиск и удаление остальных элементов
+	}
 } // Удаляет все элементы из списка по значению
 
 func (l *List) GetByIndex(id int64) (value int64, ok bool) {
 	if id < 0 || id >= l.len {
-		return 0, false // Возвращаем 0 и false, если индекс недопустим
+		return 0, false
 	}
 
 	current := l.firstNode
@@ -119,7 +110,6 @@ func (l *List) GetByIndex(id int64) (value int64, ok bool) {
 	}
 
 	return current.value, true
-	// Возвращаем значение и true, если элемент найден
 } // Возвращает значение элемента по индексу.
 
 func (l *List) GetByValue(value int64) (id int64, ok bool) {
@@ -129,13 +119,12 @@ func (l *List) GetByValue(value int64) (id int64, ok bool) {
 	for current != nil {
 		if current.value == value {
 			return id, true
-		} // Возвращаем значение и true, если элемент найден
+		}
 		current = current.next
 		id++
 	}
 
 	return 0, false
-	// Возвращаем 0 и false, если элемент с указанным значением не найден
 } // Возвращает индекс первого найденного элемента по значению.
 
 func (l *List) GetAllByValue(value int64) (ids []int64, ok bool) {
@@ -153,13 +142,11 @@ func (l *List) GetAllByValue(value int64) (ids []int64, ok bool) {
 	}
 
 	return nil, false
-	/* Возвращаем nil и false,
-	если элементы с указанным значением не найдены*/
 } // Возвращает индексы всех найденных элементов по значению
 
 func (l *List) GetAll() (values []int64, ok bool) {
 	if l.len == 0 {
-		return nil, false // Возвращаем nil и false, если список пуст
+		return nil, false
 	}
 
 	values = make([]int64, l.len)
