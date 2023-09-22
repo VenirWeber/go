86eea24b1bd980a60b96f9297af8e1a0926f977b
Denis Smirnov <113039356+VenirWeber@users.noreply.github.com>
2023-09-22 13:40:11 +0500

    Update main.go

--- storage/main.go
+++ storage/main.go
@@ -1,15 +1,4 @@
-package main
-
-import (
-	list "list/storage"
-)
+package main
 
 func main() {
-	l := list.NewList()
-	l.Add(11)
-	l.Add(12)
-	l.Add(13)
-	l.Add(14)
-	l.RemoveByIndex(3)
-	l.Print()
 }
