76875a5285b07b854dff679b3e6212d50f277439
Denis Smirnov <113039356+VenirWeber@users.noreply.github.com>
2023-10-24 16:34:26 +0300

    Update main.go

--- list/main.go
+++ list/main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"list/storage/list"
+	lis "list/storage/list"
 	"list/storage/mp"
 )
 
 func main() {
-	l := list.NewList()
-	l.Add(11)
-	l.Add(12)
-	l.Add(13)
-	l.Add(14)
-	l.RemoveByIndex(3)
-
+	l := lis.NewList()
 	m := mp.NewMap()
-	m.Add(100)
-	m.Add(200)
-	m.Add(300)
-
-	m.Print()
-
 }
